709a3476f4b1a2418a10438a8266a343407fa71d
RakhKaran <132744751+RakhKaran@users.noreply.github.com>
2026-07-29 17:16:47 +0530

    feat(customer-repository): enhance customer repository to support many-to-many relationship with customer labels and assignments

--- src/repositories/customer.repository.ts
+++ src/repositories/customer.repository.ts
@@ -1,10 +1,11 @@
 import {Constructor, Getter, inject} from '@loopback/core';
-import {BelongsToAccessor, DefaultCrudRepository, repository} from '@loopback/repository';
+import {BelongsToAccessor, DefaultCrudRepository, HasManyThroughRepositoryFactory, repository} from '@loopback/repository';
 import {PresstoDataSource} from '../datasources';
 import {TimeStampRepositoryMixin} from '../mixins/timestamp-repository-mixin';
-import {Customer, CustomerRelations, Users, CustomerLabel} from '../models';
+import {Customer, CustomerRelations, Users, CustomerLabel, CustomerLabelAssignment} from '../models';
 import {UsersRepository} from './users.repository';
 import {CustomerLabelRepository} from './customer-label.repository';
+import {CustomerLabelAssignmentRepository} from './customer-label-assignment.repository';
 
 export class CustomerRepository extends TimeStampRepositoryMixin<
   Customer,
@@ -19,15 +20,26 @@ export class CustomerRepository extends TimeStampRepositoryMixin<
 >(DefaultCrudRepository) {
   public readonly user: BelongsToAccessor<Users, typeof Customer.prototype.id>;
 
-  public readonly customerLabel: BelongsToAccessor<CustomerLabel, typeof Customer.prototype.id>;
+  public readonly customerLabels: HasManyThroughRepositoryFactory<
+    CustomerLabel,
+    typeof CustomerLabel.prototype.id,
+    CustomerLabelAssignment,
+    typeof Customer.prototype.id
+  >;
 
   constructor(
     @inject('datasources.pressto') dataSource: PresstoDataSource,
-    @repository.getter('UsersRepository') protected usersRepositoryGetter: Getter<UsersRepository>, @repository.getter('CustomerLabelRepository') protected customerLabelRepositoryGetter: Getter<CustomerLabelRepository>,
+    @repository.getter('UsersRepository') protected usersRepositoryGetter: Getter<UsersRepository>,
+    @repository.getter('CustomerLabelRepository') protected customerLabelRepositoryGetter: Getter<CustomerLabelRepository>,
+    @repository.getter('CustomerLabelAssignmentRepository') protected customerLabelAssignmentRepositoryGetter: Getter<CustomerLabelAssignmentRepository>,
   ) {
     super(Customer, dataSource);
-    this.customerLabel = this.createBelongsToAccessorFor('customerLabel', customerLabelRepositoryGetter,);
-    this.registerInclusionResolver('customerLabel', this.customerLabel.inclusionResolver);
+    this.customerLabels = this.createHasManyThroughRepositoryFactoryFor(
+      'customerLabels',
+      customerLabelRepositoryGetter,
+      customerLabelAssignmentRepositoryGetter,
+    );
+    this.registerInclusionResolver('customerLabels', this.customerLabels.inclusionResolver);
     this.user = this.createBelongsToAccessorFor('user', usersRepositoryGetter);
     this.registerInclusionResolver('user', this.user.inclusionResolver);
   }
